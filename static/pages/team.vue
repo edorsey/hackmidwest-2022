b66e7fb563eac9693e63bbfe24de3352ed57b4c3
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2022-07-23 20:47:31 -0500

    udpate static site

--- static/pages/team.vue
+++ static/pages/team.vue
@@ -21,9 +21,9 @@
               <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
                   <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                  <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">About</a>
+                  <a href="/#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
 
-                  <a href="team" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                  <a href="/team" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Team</a>
 
                   <!-- <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
@@ -103,10 +103,17 @@
       <div class="border-b border-gray-700 md:hidden" id="mobile-menu">
         <div class="px-2 py-3 space-y-1 sm:px-3">
           <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-          <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
-          <a href="/team" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+          <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">About</a>
+
+          <a href="team" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+
+          <!-- <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+
+          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+
+          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Reports</a> -->
         </div>
-        
+
       </div>
     </nav>
     <header class="py-10">
@@ -121,7 +128,48 @@
       <!-- Replace with your content -->
       <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
         <!-- <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div> -->
-        <About />
+        <div class="bg-gray-900">
+    <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+      <div class="space-y-12">
+        <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+          <h2 class="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">Meet our team</h2>
+          <!-- <p class="text-xl text-gray-300">This is our team.</p> -->
+        </div>
+        <ul role="list" class="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 lg:gap-8">
+          <li v-for="person in people" :key="person.name" class="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
+            <div class="space-y-6 xl:space-y-10">
+              <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" :src="person.imageUrl" alt="" />
+              <div class="space-y-2 xl:flex xl:items-center xl:justify-between">
+                <div class="font-medium text-lg leading-6 space-y-1">
+                  <h3 class="text-white">{{ person.name }}</h3>
+                  <p class="text-indigo-400">{{ person.role }}</p>
+                </div>
+
+                <ul role="list" class="flex justify-center space-x-5">
+                  <li>
+                    <a :href="person.twitterUrl" class="text-gray-400 hover:text-gray-300">
+                      <span class="sr-only">Twitter</span>
+                      <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a :href="person.linkedinUrl" class="text-gray-400 hover:text-gray-300">
+                      <span class="sr-only">LinkedIn</span>
+                      <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
       </div>
       <!-- /End replace -->
     </div>
@@ -130,8 +178,31 @@
 
 </template>
 
-<script>
-export default {
-  name: 'Home'
-}
+<script setup>
+const people = [
+  {
+    name: 'Eric Dorsey',
+    role: 'Principal Engineer',
+    imageUrl:
+      'https://media-exp1.licdn.com/dms/image/C5603AQELgF7j7Ta5rg/profile-displayphoto-shrink_800_800/0/1634704727342?e=1664409600&v=beta&t=tTtNLi1U_6LHWM2oxH62Bc4u6CaUARXk_i7qcMEvfeE',
+    twitterUrl: 'https://twitter.com/edorsey',
+    linkedinUrl: 'https://www.linkedin.com/in/ericbdorsey/',
+  },
+  {
+    name: 'Patrick Ecord',
+    role: 'Principal Engineer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    twitterUrl: '#',
+    linkedinUrl: 'https://www.linkedin.com/in/patrick-ecord-76bb0375/',
+  },
+  {
+    name: 'Aldon Isenberg',
+    role: 'Software Engineer',
+    imageUrl:
+      'https://media-exp1.licdn.com/dms/image/C4E03AQGQYQfBvI4XOA/profile-displayphoto-shrink_800_800/0/1589565788714?e=1664409600&v=beta&t=FRTUaLt3HlSFfIoXdfkN6eD4G4eS2qg9oHK_QUY6O-c',
+    twitterUrl: '#',
+    linkedinUrl: 'https://www.linkedin.com/in/aldon-isenberg-ii-137242115/',
+  },
+]
 </script>
